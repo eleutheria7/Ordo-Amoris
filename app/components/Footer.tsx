7a92ff2337fb44a6d28aa5046e6de2c771de9750
eleutheria7 <jovenseleutheria@gmail.com>
2025-11-28 23:16:03 -0300

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="bg-[#447a37] text-white py-8 px-4">
+    <footer className="bg-[#225c1c] text-white py-8 px-4">
       <div className="max-w-4xl mx-auto flex flex-col md:flex-row items-center justify-between">
         <img
           src="/assets/img/paroquia.png"
